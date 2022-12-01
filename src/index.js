5fed0640d1ad4b838dca56e9fb4d02d736f0794d
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2022-12-01 11:00:51 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -22,9 +22,16 @@ function verifyIfExistsAccountCPF(request, response, next) {
 }
 
 function getBalance(statement) {
-  
-}
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    }else {
+      return acc - operation.amount;
+    }
+  }, 0);
 
+  return balance;
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -69,7 +76,64 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   
   customer.statement.push(statementOperation);
   
+  return response.ststus(201).send();
 })
 
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
 
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient Funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() === new Date (dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+   
+})
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customers, 1);
+
+  return response.ststus(200).json(customers);
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+})
 app.listen(4000);
